1b414945740a3cecf9fcb6b8c4fdb46f72bd774f
samjang9509 <samjang9509@github.com>
2020-06-16 12:59:07 +0900

    questions

---
+++ build/questions/CMakeFiles/questions_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/questions_node.dir/src/main.cpp.o"
+  "/home/cona/sam/Baekjun/devel/lib/questions/questions_node.pdb"
+  "/home/cona/sam/Baekjun/devel/lib/questions/questions_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/questions_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/questions/src/2557.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    printf("Hello World!\n");
+
+    return 0;
+
+}
+
