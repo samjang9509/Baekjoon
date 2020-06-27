29db6924c962d60796947fd0b11605e21cbd72bd
samjang9509 <samjang9509@github.com>
2020-06-27 16:39:48 +0900

    questions

--- src/questions/src/main.cpp
+++ src/questions/src/main.cpp
@@ -3,33 +3,31 @@
 int main(int agrc, char **argv)
 {
     int a = 0;
-    
     std::cin >> a;
-    
-    int down = 2*a - 1;
-    int count = a - 1;
 
-    for (int i = 1; i <= a; i++)
+    int down = a - 1;
+
+    for ( int i = 0; i < a; i++)
     {
-        for(int j = a-1; j >= down; x--)
+        for(int j = 0; j < i; j++)
         {
             std::cout << " ";
         }
-
-        for(int x=1; x<=b; x++)
-        {
-            std::cout << "*";
-        }]
-        std::cout << std::endl;
-    }
-
-    for (int k = 1; k <= down - a; k++)
-    {
-        for (int i = a; i <= 0; i++)
+        for(int k = 0; k < 2 * (a-i) - 1; k++ )
         {
             std::cout << "*";
         }
-        std::cout << std::endl;
-        count--;
+        std::cout << '\n';     
     }
+
+    for(int i = 0; i < down; i++)
+    {
+        for(int j = 0; j < 2 * a - 1; j++)
+        {
+            
+        }
+    }
+
+
+
 }
