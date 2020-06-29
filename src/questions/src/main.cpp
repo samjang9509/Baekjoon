64ae261b64f5d81776e6c532a371546b9e86382d
samjang9509 <samjang9509@gmail.com>
2020-06-29 14:23:57 +0900

    questions

--- src/questions/src/main.cpp
+++ src/questions/src/main.cpp
@@ -1,33 +1,56 @@
 #include <iostream>
+using namespace std;
 
-int main(int agrc, char **argv)
+int main()
 {
-    int a = 0;
-    std::cin >> a;
-
-    int down = a - 1;
-
-    for ( int i = 0; i < a; i++)
-    {
-        for(int j = 0; j < i; j++)
-        {
-            std::cout << " ";
-        }
-        for(int k = 0; k < 2 * (a-i) - 1; k++ )
-        {
-            std::cout << "*";
-        }
-        std::cout << '\n';     
-    }
-
-    for(int i = 0; i < down; i++)
-    {
-        for(int j = 0; j < 2 * a - 1; j++)
-        {
-            
-        }
-    }
-
-
-
+	int n = 0;
+	cin >> n;
+	if (n == 1)
+		cout << "*"<<"\n";
+	else
+	{
+		for (int i = 1; i <= 2 * n; i++)
+		{
+			if (i % 2 != 0)//홀수줄
+			{
+				if (n % 2 != 0)//홀수정수
+				{
+					for (int j = 0; j < n / 2 + 1; j++)
+					{
+						cout << "*";
+						cout << " ";
+					}
+				}
+				else
+				{
+					for (int j = 0; j < n / 2; j++)
+					{
+						cout << "*";
+						cout << " ";
+					}
+				}
+			}
+			else//짝수줄
+			{
+				if (n % 2 != 0)//홀수정수
+				{
+					for (int j = 0; j < n / 2 + 1; j++)
+					{
+						cout << " ";
+						cout << "*";
+					}
+				}
+				else
+				{
+					for (int j = 0; j < n / 2; j++)
+					{
+						cout << " ";
+						cout << "*";
+					}
+				}
+			}
+			cout << "\n";
+		}
+	}
+	return 0;
 }
---
+++ src/questions/src/practise1/2446.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int main(int agrc, char **argv)
+{
+    int a = 0;
+    std::cin >> a;
+
+    int down = a - 1;
+
+    for ( int i = 0; i < a; i++)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            std::cout << " ";
+        }
+        for(int k = 0; k < 2 * (a-i) - 1; k++ )
+        {
+            std::cout << "*";
+        }
+        std::cout << '\n';     
+    }
+
+    
+    for(int i = 1; i <= down; i++)
+    {
+        for(int j = 0; j < down - i; j++)
+        {
+            std::cout << " ";
+        }
+        for(int k = 0; k < 2 * i + 1; k++)
+        {
+            std::cout << "*";
+        }
+        std::cout << '\n';
+    }
+
+
+
+}
